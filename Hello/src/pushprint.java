b7da5f9cb9dce867a40c7e4af67a5add2fa0d090
jycc0405 <121859759+jycc0405@users.noreply.github.com>
2023-04-05 10:02:04 +0900

    motifed on github

--- Hello/src/pushprint.java
+++ Hello/src/pushprint.java
@@ -4,7 +4,7 @@ public class pushprint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("add java file");
-
+		System.out.print("Hello");
 		
 		
 		
